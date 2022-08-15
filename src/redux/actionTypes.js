49fc3effee8d273edff5adc3ceb9155e7f5732f3
OkoyeCharles <okoyecharles509@gmail.com>
2022-08-15 14:28:20 +0100

    Define action types

---
+++ src/redux/actionTypes.js
@@ -0,0 +1,8 @@
+// CONTAINS PREDEFINED ACTION TYPES FOR CLARITY.
+
+// Books
+export const ADD_BOOK = "ADD_BOOK";
+export const REMOVE_BOOK = "REMOVE_BOOK";
+
+// Categories
+export const CHECK_STATUS = "CHECK_STATUS";
