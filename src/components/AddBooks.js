5709b7757cdb4636948c97b8c98605aa21e2c459
OkoyeCharles <okoyecharles509@gmail.com>
2022-08-16 10:49:28 +0100

    Implement book creation with useDispatch

--- src/components/AddBooks.js
+++ src/components/AddBooks.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/actions';
 import '../styles/AddBooks.css';
 
 function AddBooks() {
+  const dispatch = useDispatch();
+
+  const validated = (str) => !!str.trim();
+
+  const handleAddBook = (event) => {
+    event.preventDefault();
+    const { title } = event.target.elements;
+    const { author } = event.target.elements;
+
+    if (validated(title.value) && validated(author.value)) {
+      dispatch(addBook(title.value, author.value));
+      title.value = '';
+      author.value = '';
+    }
+  };
+
   return (
-    <form className="addBooks__form">
+    <form className="addBooks__form" onSubmit={handleAddBook}>
       <h2 className="addBooks__title">ADD NEW BOOK</h2>
       <fieldset>
         <input type="text" name="title" placeholder="Book title" />
