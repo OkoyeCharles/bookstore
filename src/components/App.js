cc24b9ce551f667c645efc5058e79786f6ef459b
OkoyeCharles <okoyecharles509@gmail.com>
2022-08-16 09:35:19 +0100

    Arrange Items

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Categories from './components/Categories';
-import Home from './components/Home';
-import Nav from './components/Nav';
-import './styles/App.css';
+import Categories from './Categories';
+import Home from './Home';
+import Nav from './Nav';
+import '../styles/App.css';
 
 function App() {
   return (
