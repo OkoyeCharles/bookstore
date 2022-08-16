c9c0bab9eecce9480cbbb865e75d3d6aec6f66e3
OkoyeCharles <okoyecharles509@gmail.com>
2022-08-16 09:48:27 +0100

    Import and Connect Store to App

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from './redux/configureStore';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
+const store = configureStore({ reducer: rootReducer });
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
 
