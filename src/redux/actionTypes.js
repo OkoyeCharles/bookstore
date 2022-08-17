b760bbdaf29310149eae3114d6cdfeb66b8ed227
OkoyeCharles <okoyecharles509@gmail.com>
2022-08-17 14:10:58 +0100

    Install and apply thunk middleware to store

--- src/redux/actionTypes.js
+++ src/redux/actionTypes.js
@@ -3,6 +3,7 @@
 // Books
 export const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 export const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
+export const LOAD_BOOKS = 'bookstore/books/LOAD_BOOKS';
 
 // Categories
-export const CHECK_STATUS = 'categorystore/category/CHECK_STATUS';
+export const CHECK_STATUS = 'bookstore/category/CHECK_STATUS';
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,18 @@
-import { combineReducers } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
 import books from './books/books';
 import categories from './categories/categories';
 
-export default combineReducers({
+// Initialize Root Reducer
+const rootReducer = combineReducers({
   books,
   categories,
 });
+
+// Add Middleware (Thunk) to the reducer
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: [thunk],
+});
+
+export default store;
