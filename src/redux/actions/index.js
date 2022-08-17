3f18134864e8b896f1e77195417bcaf464e71d3c
OkoyeCharles <okoyecharles509@gmail.com>
2022-08-17 17:30:14 +0100

    Adjust crud functionality to match API request

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -11,33 +11,68 @@ let nextBookId = 0;
 //     return err.message;
 //   }
 // });
+const convertToBookArray = (axiosdata) => Object.entries(axiosdata).map((item) => ({
+  id: parseInt(item[0], 10),
+  ...item[1][0],
+})) || [];
+
 export const loadBooks = async (dispatch) => {
   const response = await axios.get(
     'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/m0ekECJs3bOXjlfILUsO/books',
   );
+  // Convert Data to usable format
+  const books = convertToBookArray(response.data);
+
   dispatch({
     type: actions.LOAD_BOOKS,
     payload: {
-      books: Object.entries(response.data),
+      books,
+    },
+  });
+  nextBookId += books.length;
+};
+
+export const addBook = (title, author) => (dispatch) => {
+  // Update store at API.
+  axios.post(
+    'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/m0ekECJs3bOXjlfILUsO/books',
+    {
+      item_id: ++nextBookId,
+      title,
+      author,
+      category: 'null',
+    },
+  );
+  // Dispatch event locally
+  dispatch({
+    type: actions.ADD_BOOK,
+    payload: {
+      id: ++nextBookId,
+      title,
+      author,
+      category: 'null',
     },
   });
 };
 
-export const addBook = (title, author) => ({
-  type: actions.ADD_BOOK,
-  payload: {
-    id: ++nextBookId,
-    title,
-    author,
-  },
-});
+export const removeBook = (id) => (dispatch) => {
+  fetch(
+    `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/m0ekECJs3bOXjlfILUsO/books/${id}`,
+    {
+      method: 'DELETE',
+      body: JSON.stringify({
+        item_id: id,
+      }),
+    },
+  ).catch((err) => console.error(err));
 
-export const removeBook = (id) => ({
-  type: actions.REMOVE_BOOK,
-  payload: {
-    id,
-  },
-});
+  dispatch({
+    type: actions.REMOVE_BOOK,
+    payload: {
+      id,
+    },
+  });
+};
 
 export const checkStatus = () => ({
   type: actions.CHECK_STATUS,
